b84dd08f37d6a49c8275a9d1038fc946e0fa172f
´´George <george.emannuelcarvalho@gmail.com>
2024-07-20 16:12:05 -0400

    created index

--- index.c
+++ index.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
+  #include <stdio.h>
 
-int main()
-{
+  int main()
+  {
 
-  int idade;
-  double salario;
-  char nome[50];
-  char sexo;
+    int idade;
+    double salario;
+    char nome[50];
+    char sexo;
 
-  idade = 23;
-  salario = 2500;
-  strcpy(nome, "Maria Silva");
-  sexo = 'F';
-  printf("A funcionaria %s, sexo %c, ganha %.2lf e tem %d anos\n", nome, sexo, salario, idade);
+    idade = 23;
+    salario = 2500;
+    strcpy(nome, "Maria Silva");
+    sexo = 'F';
+    
+    printf("A funcionaria %s, sexo %c, ganha %.2lf e tem %d anos\n", nome, sexo, salario, idade);
 
-  return 0;
-}
+    return 0;
+  }
